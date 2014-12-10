7550b0b05bd8782b3f9d22e5e8689da5b17dd1be
TheDipWagon <zc13555@students.mcpasd.k12.wi.us>
2014-12-10 10:21:27 -0600

    Daewon and Cross
    dsaf

--- prisoners_dilemma.py
+++ prisoners_dilemma.py
@@ -388,22 +388,15 @@ def get_action(player, history, opponent_history, score, opponent_score, getting
 
 
 
-    ######
+    ######Daewon Lee + Cross Z
     ######
     #
     elif player == 11:
         if getting_team_name:
-            return 'loyal vengeful'
+            return 'Dicks(Sporting Good)'
         else:
-            # use history, opponent_history, score, opponent_score
-            # to compute your strategy
-            if len(opponent_history)==0: #It's the first round: collude
-                return 'c'
-            elif history[-1]=='c' and opponent_history[-1]=='b':
-                return 'b' # betray is they were severely punished last time
-            else:
-                return 'c' #otherwise collude
-
+            return 'b'
+            #Winning
 
 
 
