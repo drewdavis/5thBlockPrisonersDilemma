a71f0dcf9da65df87a2144cbb36024f6d677f0f7
mmucks420 <ma14791@students.mcpasd.k12.wi.us>
2014-12-10 10:31:28 -0600

    Commit 7

--- prisoners_dilemma.py
+++ prisoners_dilemma.py
@@ -291,16 +291,16 @@ def get_action(player, history, opponent_history, score, opponent_score, getting
     #
     elif player == 7:
         if getting_team_name:
-            return 'loyal vengeful'
-        else:
-            # use history, opponent_history, score, opponent_score
-            # to compute your strategy
-            if len(opponent_history)==0: #It's the first round: collude
+            return 'Squaddle Squoogerz'
+        
+        if roundi == number_of_rounds:
+            return 'b'
+            size = len(history)
+            if(size%3==0): #the number of rounds played is a multiple of 3
                 return 'c'
-            elif history[-1]=='c' and opponent_history[-1]=='b':
-                return 'b' # betray is they were sucker last time
             else:
-                return 'c' #otherwise collude
+                return 'b'
+                
 
 
 
